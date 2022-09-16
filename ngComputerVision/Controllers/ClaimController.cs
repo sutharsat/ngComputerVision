bc4b99e657677002c8ecc1b4aa89b9cdf03169b9
anjan <anjan.neupane@elevancehealth.com>
2022-09-16 07:23:33 -0400

    data retrieve from mongo collection.

--- ngComputerVision/Controllers/ClaimController.cs
+++ ngComputerVision/Controllers/ClaimController.cs
@@ -2,6 +2,8 @@
 using ngComputerVision.Contracts.Entities;
 using ngComputerVision.DTOModels;
 using ngComputerVision.Repository.Interface;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace ngComputerVision.Controllers
@@ -71,10 +73,160 @@ namespace ngComputerVision.Controllers
         public async Task<ActionResult<ResultDTO>> Get(string id)
         {
             ResultDTO resultDTO = new ResultDTO();
+            //PII
+            Dictionary<string,double> personDictionary = new Dictionary<string, double>();
+            Dictionary<string, double> dateDictionary = new Dictionary<string, double>();
+            Dictionary<string, double> addressDictionary = new Dictionary<string, double>();
+            Dictionary<string, double> phonenumberDictionary = new Dictionary<string, double>();
+            Dictionary<string, double> organizationDictionary = new Dictionary<string, double>();
+            //Health
+            Dictionary<string, double> treatmentnameDictionary = new Dictionary<string, double>();
+            Dictionary<string, double> careenvironmentDictionary = new Dictionary<string, double>();
+            Dictionary<string, double> administrativeeventDictionary = new Dictionary<string, double>();
+            Dictionary<string, double> genderDictionary = new Dictionary<string, double>();
+            Dictionary<string, double> healthcareprofessionDictionary = new Dictionary<string, double>();
             var entityHealthResult = await _entityRepository.GetHealthEntityWithId(id);
             var PIIResult = await _PIIRepository.GetPIIResultWithId(id);
+            foreach (PII pII in PIIResult.PIIEntities)
+            {
+                if (pII.Category.Equals("Person"))
+                {
+                    if (!personDictionary.ContainsKey(pII.Text))
+                    {
+                        personDictionary.Add(pII.Text, pII.ConfidenceScore);
+                    }
 
-            return resultDTO;
+                }
+
+                else if (pII.Category.Equals("DateTime"))
+                {
+                    if (!dateDictionary.ContainsKey(pII.Text))
+                    {
+                        dateDictionary.Add(pII.Text, pII.ConfidenceScore);
+                    }
+                }
+                else if (pII.Category.Equals("Address"))
+                {
+                    if (!addressDictionary.ContainsKey(pII.Text))
+                    {
+                        addressDictionary.Add(pII.Text, pII.ConfidenceScore);
+                    }
+                }
+                else if (pII.Category.Equals("PhoneNumber"))
+                {
+                    if (!phonenumberDictionary.ContainsKey(pII.Text))
+                    {
+                        phonenumberDictionary.Add(pII.Text, pII.ConfidenceScore);
+                    }
+                }
+                else if (pII.Category.Equals("Organization"))
+                {
+                    if (!organizationDictionary.ContainsKey(pII.Text))
+                    {
+                        organizationDictionary.Add(pII.Text, pII.ConfidenceScore);
+
+                    }
+                }
+            }
+            foreach (Entity entity in entityHealthResult.entities)
+            {
+                if (entity.Category.Equals("TreatmentName"))
+                {
+                    if (!treatmentnameDictionary.ContainsKey(entity.Text))
+                    {
+                        treatmentnameDictionary.Add(entity.Text, entity.ConfidenceScore);
+
+                    }
+                }
+
+                else if (entity.Category.Equals("Gender"))
+                {
+                    if (!genderDictionary.ContainsKey(entity.Text))
+                    {
+                        genderDictionary.Add(entity.Text, entity.ConfidenceScore);
+
+                    }
+                }
+                else if (entity.Category.Equals("CareEnvironment"))
+                {
+                        if (!careenvironmentDictionary.ContainsKey(entity.Text))
+                        {
+                            careenvironmentDictionary.Add(entity.Text, entity.ConfidenceScore);
+
+                        }
+                    }
+                else if (entity.Category.Equals("AdministrativeEvent"))
+                {
+                            if (!administrativeeventDictionary.ContainsKey(entity.Text))
+                            {
+                                administrativeeventDictionary.Add(entity.Text, entity.ConfidenceScore);
+
+                            }
+                        }
+                else if (entity.Category.Equals("HealthcareProfession"))
+                {
+                                if (!healthcareprofessionDictionary.ContainsKey(entity.Text))
+                                {
+                                    healthcareprofessionDictionary.Add(entity.Text, entity.ConfidenceScore);
+                                }
+                                }
+            }
+            //For PII
+            if (personDictionary.Count >= 1)
+            {
+                resultDTO.firstname = personDictionary.ElementAt(0).Key;
+                resultDTO.firstnamecs = personDictionary.ElementAt(0).Value;
+
+                resultDTO.lastname = personDictionary.ElementAt(1).Key;
+                resultDTO.lastnamecs = personDictionary.ElementAt(1).Value;
+            }
+            if (phonenumberDictionary.Count >= 1)
+            {
+                resultDTO.phonenumber = phonenumberDictionary.ElementAt(0).Key;
+                resultDTO.phonenumbercs = phonenumberDictionary.ElementAt(0).Value;
+            }
+            if (dateDictionary.Count >= 1)
+            {
+                resultDTO.dateofbirth = dateDictionary.ElementAt(0).Key;
+                resultDTO.dateofbirthcs = dateDictionary.ElementAt(0).Value;
+            }
+            if (addressDictionary.Count >= 1)
+            {
+                resultDTO.address = addressDictionary.ElementAt(0).Key;
+                resultDTO.addresscs = addressDictionary.ElementAt(0).Value;
+            }
+            if (organizationDictionary.Count >= 1)
+            {
+                resultDTO.hospitalname = organizationDictionary.ElementAt(0).Key;
+                resultDTO.hospitalnamecs = organizationDictionary.ElementAt(0).Value;
+            }
+            //For Health
+            if (treatmentnameDictionary.Count >= 1)
+            {
+                resultDTO.treatmentname = treatmentnameDictionary.ElementAt(0).Key;
+                resultDTO.treatmentnamecs = treatmentnameDictionary.ElementAt(0).Value;
+            }
+            if (healthcareprofessionDictionary.Count >= 1)
+            {
+                resultDTO.healthcareprofession = healthcareprofessionDictionary.ElementAt(0).Key;
+                resultDTO.healthcareprofessioncs = healthcareprofessionDictionary.ElementAt(0).Value;
+            }
+            if (careenvironmentDictionary.Count >= 1)
+            {
+                resultDTO.careenvironment = careenvironmentDictionary.ElementAt(0).Key;
+                resultDTO.careenvironmentcs = careenvironmentDictionary.ElementAt(0).Value;
+            }
+            if (administrativeeventDictionary.Count >= 1)
+            {
+                resultDTO.administrativeevent = administrativeeventDictionary.ElementAt(0).Key;
+                resultDTO.administrativeeventcs = administrativeeventDictionary.ElementAt(0).Value;
+            }
+            if (genderDictionary.Count >= 1)
+            {
+                resultDTO.gender = genderDictionary.ElementAt(0).Key;
+                resultDTO.gendercs = genderDictionary.ElementAt(0).Value;
+            }
+                return resultDTO;
         }
 
 
--- ngComputerVision/DTOModels/ResultDTO.cs
+++ ngComputerVision/DTOModels/ResultDTO.cs
@@ -2,16 +2,33 @@
 {
     public class ResultDTO
     {
+        //PII
         public string firstname { get; set; }
+        public double firstnamecs { get; set; }
         public string lastname { get; set; }
+        public double lastnamecs { get; set; }
         public string dateofbirth { get; set; }
+        public double dateofbirthcs { get; set; }
         public string address { get; set; }
-        public string gender { get; set; }
-        public string ptan { get; set; }
-        public int medicareID { get; set; }
-        public int npi { get; set; }
+        public double addresscs { get; set; }
+        // public string gender { get; set; }
+        // public string ptan { get; set; }
+        // public int medicareID { get; set; }
+        // public int npi { get; set; }
         public string phonenumber { get; set; }
-
+        public double phonenumbercs { get; set; }
         public string hospitalname { get; set; }
+        public double hospitalnamecs { get; set; }
+        //Health
+        public string treatmentname { get; set; }
+        public double treatmentnamecs { get; set; }
+        public string gender { get; set; }
+        public double gendercs { get; set; }
+        public string careenvironment { get; set; }
+        public double careenvironmentcs { get; set; }
+        public string administrativeevent { get; set; }
+        public double administrativeeventcs { get; set; }
+        public string healthcareprofession { get; set; }
+        public double healthcareprofessioncs { get; set; }
     }
 }
